68e2d056b747a00a2fa11b3e4fcf6b86eada99ae
zahran-musyaffa <zahran.musyaffa@ui.ac.id>
2026-04-14 10:49:06 +0700

    Implement unsubscribe function in Notification controller

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -5,6 +5,6 @@ pub mod notification;
 pub fn route_stage() -> AdHoc {
     return AdHoc::on_ignite("Initializing controller routes...", |rocket| async {
         rocket
-            .mount("/", routes![notification::subscribe])
+            .mount("/", routes![notification::subscribe, notification::unsubscribe])
     });
 }
