ad4ddf985a85e94ff41d7e105550f9ee36c2d793
zahran-musyaffa <zahran.musyaffa@ui.ac.id>
2026-04-14 10:45:36 +0700

    Implement add function in Subscriber repository

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -12,4 +12,14 @@ lazy_static! {
 pub struct NotificationRepository;
 
 impl NotificationRepository {
+    pub fn add(notification: Notification) -> Notification {
+        NOTIFICATIONS.write().unwrap()
+            .push(notification.clone());
+        return notification;
+    }
+
+    pub fn list_all_as_string() -> Vec<String> {
+        return NOTIFICATIONS.read().unwrap()
+            .iter().map(|f| format!("{}", f.clone())).collect();
+    }
 }
